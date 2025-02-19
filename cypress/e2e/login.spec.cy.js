9b19f1e7aba8af5f135ccca553d49bf7d1aceec8
seu-usersaneme <seu-emailit push -u origin maingitcei9dned=iajftgnh>
2025-02-19 14:59:53 -0300

    Added selectors list for login spec

--- cypress/e2e/login.spec.cy.js
+++ cypress/e2e/login.spec.cy.js
@@ -1,17 +1,27 @@
+
+
+const selectorslist = {
+  usernameField: "[name='username']",
+  passwordField: "[name='password']",
+  loginButton: "[type='submit']",
+  sectionTitleTopBar:".oxd-topbar-header-breadcrumb-module",
+  wrongCredentialAlert: "[role='alert']",
+}
+
 describe('Orange HRM Tests', () => {
   it('Login - Success', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-    cy.get("[name='username']").type ('Admin')
-    cy.get("[name='password']").type ('admin123')
-    cy.get("[type='submit']").click()
+    cy.get(selectorslist.usernameField).type ('Admin')
+    cy.get(selectorslist.passwordField).type ('admin123')
+    cy.get(selectorslist.loginButton).click()
     cy.location('pathname').should('equal', '/web/index.php/dashboard/index')
-    cy.get(".oxd-topbar-header-breadcrumb-module").contains('Dashboard')
+    cy.get(selectorslist.sectionTitleTopBar).contains('Dashboard')
   })
   it('Login - Fail', () => {
       cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-      cy.get("[name='username']").type ('Admin')
-      cy.get("[name='password']").type ('admin123')
-      cy.get("[type='submit']").click()
-      cy.get("[role='alert']")      
+      cy.get(selectorslist.usernameField).type ('Admin')
+      cy.get(selectorslist.passwordField).type ('admin123')
+      cy.get(selectorslist.loginButton).click()
+      cy.get(selectorslist.wrongCredentialAlert)      
   })
   })
